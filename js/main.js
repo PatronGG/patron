538c5c713038e1d6a3fb3fa1329a65197ad457e0
Liam D <liamdocuk@gmail.com>
2018-06-19 10:29:13 +0200

    update text / pictures

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 let cards = [
   {
     photo: './images/placeholder.png',
+    game: './images/league-logo.png',
     name: 'Dean Burrows',
     title: 'CEO, Head of Product',
     text: 'Built awesome products at ChallengeMe.GG and Hubrick',
@@ -11,22 +12,25 @@ let cards = [
   },
   {
     photo: './images/bitkraft-logo.png',
+    game: './images/csgo-logo.png',
     name: 'Jens Hilgers',
-    title: 'Founder, Advisor',
+    title: 'Advisor',
     text: 'The \'Godfather of Esports\'. Built ESL, BITKRAFT & Dojo Madness',
     logos: [`<img src="./images/esl-logo.png">`, `<img src="./images/bitkraft-logo.png">`],
     dots: `<div class="dot"></div><div class="dot-solid"></div></div><div class="dot"></div></div><div class="dot"></div></div><div class="dot"></div></div><div class="dot"></div>`
   },
   {
     photo: './images/placeholder.png',
+    game: './images/league-logo.png',
     name: 'Michael Arnold',
-    title: 'Founder, Advisor',
+    title: 'Advisor',
     text: 'Founding team @ NeoReach (SF Based Startup). Also Wooga.',
-    logos: [`<img src="./images/hubrick-logo.jpg">`],
+    logos: [`<img src="./images/neoreach-logo.png"><img src="./images/wooga-Logo.png">`],
     dots: `<div class="dot"></div><div class="dot"></div></div><div class="dot-solid"></div></div><div class="dot"></div></div><div class="dot"></div></div><div class="dot"></div>`
   },
   {
     photo: './images/bitkraft-logo.png',
+    game: './images/fifa-logo.png',
     name: 'Dominic Graefin',
     title: 'Lead Developer',
     text: '10+ years experience. Built games at Wooga for 50M monthly active users.',
@@ -35,6 +39,7 @@ let cards = [
   },
   {
     photo: './images/placeholder.png',
+    game: './images/dota-logo.png',
     name: 'Gustavo Brunoro',
     title: 'Senior Developer',
     text: '6+ years of experience building scalable mobile apps, worked at Wooga.',
@@ -43,10 +48,11 @@ let cards = [
   },
   {
     photo: './images/bitkraft-logo.png',
+    game: './images/league-logo.png',
     name: 'Elliot Rarden',
     title: 'Junior Developer',
     text: '4+ years of experience in JS/Android development. Worked at Echostar.',
-    logos: [`<img src="./images/wooga-logo.png">`],
+    logos: [`<img src="./images/echostar-logo.jpeg">`],
     dots: `<div class="dot"></div><div class="dot"></div></div><div class="dot"></div></div><div class="dot"></div></div><div class="dot"></div></div><div class="dot-solid"></div>`
   },
 ];
@@ -82,21 +88,41 @@ function toggle() {
 
 
 
-// EMAIL SUBMIT
-const submit = document.getElementById('submit');
-const input = document.getElementById('mailInput');
+// // EMAIL SUBMIT
+// const submit = document.getElementById('submit');
+// const input = document.getElementById('mailInput');
 
-submit.addEventListener('click', mailListAdd);
+// submit.addEventListener('click', mailListAdd);
 
-function mailListAdd(e) {
-  e.preventDefault();
-  let regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+// function mailListAdd(e) {
+//   e.preventDefault();
+//   let regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-  if(input.value.match(regex)) {
-    console.log(input.value);
-    input.value = "";
-  }
-}
+//   if(input.value.match(regex)) {
+//     addMail(input.value);
+//     input.value = "";
+//   }
+// }
+
+// function addMail(email) {
+//   let req = {
+//     "email_address": email,
+//     "status": "subscribed"
+//   }
+
+// let url = `https://patron.us18.list-manage.com/subscribe/post-json?u=28b112e4bfcc1f15428ffcda8&id=7c29065922&`
+
+// //https://patron.us14.list-manage.com/subscribe/post-json?u=fce559a91e6e59aa5d1e9f18d&id=72ed5a981f
+//   fetch('https://us18.api.mailchimp.com/3.0/lists/9e67587f52/members/', {
+//     method: 'post',
+//     body: JSON.stringify(req),
+
+//   }).then(function (data) {  
+//     console.log('Request success: ', data);  
+//   }).catch(function (error) {  
+//     console.log('Request failure: ', error);  
+//   });
+// }
 
 
 
@@ -106,9 +132,9 @@ const navBar = document.querySelector('nav');
 
 function fadeIn() {
   var scrolled = header.getBoundingClientRect().top;
-  if(scrolled <= -300) {
+  if(scrolled <= -30) {
     navBar.style.background = "#424659";
-  } else if (scrolled > -300) {
+  } else if (scrolled > -30) {
     navBar.style.background = "none";
   }
 }
@@ -122,6 +148,7 @@ const btnLeft = document.getElementById('slider-right');
 let x = 0;
 
 let photo = document.getElementById('slider-photo');
+let game = document.getElementById('slider-game');
 let name = document.getElementById('slider-name');
 let title = document.getElementById('slider-title');
 let text = document.getElementById('slider-text');
@@ -131,6 +158,7 @@ let dots = document.querySelector('.dots');
 function update() {
   photo.style.background = `url(${cards[x].photo}) center center no-repeat`;
   photo.style.backgroundSize = 'cover';
+  game.src = cards[x].game;
   name.innerHTML = cards[x].name;
   title.innerHTML = cards[x].title;
   text.innerHTML = cards[x].text;
